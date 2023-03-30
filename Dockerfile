a1c1ae8d220705884de05c758854bf15ffe70cfd
chang <kennethchangla@gmail.com>
2023-03-30 14:53:55 +0800

    [ release ] set up environment fot prototype

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,11 @@ COPY ./package*.json ./
 RUN npm ci
 COPY . .
 
+# Add wait-for-it script
+ADD https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh /usr/local/bin/wait-for-it
+RUN chmod +x /usr/local/bin/wait-for-it
+
 RUN npm install -g sequelize-cli
-CMD ["sh", "-c", "sequelize db:migrate && npm start"]
+CMD ["sh", "-c", "wait-for-it mysqldb:3307 -- sequelize db:migrate && npm start"]
 
 # CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = require('./src/routes/router');
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/initDB');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
+
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.listen(9000);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,9 +21,14 @@ app.use('/', router);
 app.set('port', process.env.PORT || 3020);
 
 // sequelize.sync().then(() => {
-	const server = app.listen(app.get('port'), () => {
-		console.log('Listening on port ' + server.address().port);
-	})
+const server = app.listen(app.get('port'), () => {
+    console.log('Listening on port ' + server.address().port);
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+  
 
 // }).catch(err => {
 //     console.error('Unable to connect to the database:', err);
