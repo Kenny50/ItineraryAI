5b0d160b4cffa9744669d9a7d205833701a520ce
chang <kennethchangla@gmail.com>
2023-03-31 21:20:50 +0800

    [ deploy ] using nginx for web and server, change mysql store to local

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors())
-app.use('/', router);
+app.use('/api/', router);
 
 app.set('port', process.env.PORT || 3020);
 
