10eeca32373b6b5bfac572c1dcc37efa90135970
chang <kennethchangla@gmail.com>
2023-05-05 16:46:29 +0800

    update nodejs docker setup

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 const express = require('express');
-const router = require('./src/routes/router');
-const bodyParser = require('body-parser')
-const sequelize = require('./src/db/initDB');
 const cors = require('cors');
-const path = require('path');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const constant = require('./src/constant/constant');
+const router = require('./src/routes/router');
+const logger = require('./src/middleware/logger');
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
+app.use(constant.staticFileDir, express.static('/app/config'));
 app.use(cors())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://itineraryai.com');
+    res.setHeader('Access-Control-Allow-Origin', ['http://itineraryai.com', 'http://localhost']);
     next();
 });
+
+app.use(logger);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(multer().array()); 
+
 app.use('/api/', router);
 
 app.set('port', process.env.PORT || 3020);
 
-// sequelize.sync().then(() => {
 const server = app.listen(app.get('port'), () => {
     console.log('Listening on port ' + server.address().port);
 })
-
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-  
-
-// }).catch(err => {
-//     console.error('Unable to connect to the database:', err);
-// });
